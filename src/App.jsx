b0d95f2b83cd5987d261267add6779b759f0c831
Dsmalldara <100885837+Dsmalldara@users.noreply.github.com>
2023-04-04 18:22:16 +0100

    commit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { db } from "./Firebase-config";
-import {collection, getDocs} from 'firebase/firestore'
+import {collection, getDocs, addDoc, updateDoc, doc} from 'firebase/firestore'
 function App(){
   const [users, setUsers] = useState([])
   const getUsersRef = collection(db, "Users")
+  const [userName, setUserName] = useState("");
+  const [age, setAge] = useState(0)
+  const Update = async ()=>{
+    await addDoc ( getUsersRef,{name: userName, age : age})
+  };
+  const updateAge = async(id, age)=>{
+    const userRef = doc (db, "Users", id)
+     const addAge = {age : age + 1}
+    await updateDoc(userRef, addAge)
+  }
   useEffect(()=>{
      const getUsers = async()=>{
      const data = await getDocs(getUsersRef)
@@ -14,13 +24,19 @@ function App(){
       getUsers();
     },[])
   return (
-    <div>
+    <div style={{marginLeft:"6rem", marginTop:"10rem"}}>
+      <input type="text" placeholder="Input Name" onChange={(event)=>{setUserName(event.target.value)}}/>
+      <br></br>
+      <input type="number" placeholder="Input Age" onChange={(event)=>{setAge(event.target.value)}}/>
+      <button onClick={Update}>Add New profile</button>
+      
       {users.map((user)=>{
   return (
     <div>
-          {" "}
-         <h1>Name: {user.Name}</h1>
-         <h1>Age: {user.Age}</h1>
+        
+         <h1>Name: {user.name}</h1>
+         <h1>Age: {user.age}</h1>
+         <button onClick={updateAge(user.id, user.age)}>Increase Age</button>
     </div>
   )
       })}
